44833f0a443e733eb895834b6edc129d056e1f01
Julia <emili@gmail,com>
2025-02-05 19:23:14 +0300

    Добавила Пр15

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,33 @@
+﻿namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static bool Same(int number) //Проверка равенства
+        {
+            int first = number / 100; // Получаем первую цифру
+            int second = (number / 10) % 10; // Получаем вторую цифру
+            int third = number % 10; // Получаем третью цифру
+            return first == second && second == third;
+        }
+        public static int Product(int number) //Вычисление произведения
+        {
+            int first = number / 100;
+            int second = (number / 10) % 10;
+            int third = number % 10;
+            return first * second * third;
+        }
+        public static bool ProductThree(int number) //Проверка на трёхзначначность произведения 
+        {
+            int product = Product(number);
+            return product >= 100 && product <= 999;
+        }
+        public static bool SumA(int number, int A) //Проверка на кратность
+        {
+            int first = number / 100;
+            int second = (number / 10) % 10;
+            int third = number % 10;
+            int sum = first + second + third;
+            return sum % A == 0;
+        }
+    }
+}
---
+++ Практическая 15/Program.cs
@@ -0,0 +1,26 @@
+﻿/* 4. Ввести трёхзначное число. 
+а) верно ли, что все его цифры одинаковые?
+б) является ли произведение его цифр трёхзначным числом;
+в) кратна ли сумма его цифр введенному числу A. */
+
+using ClassLibrary1;
+
+namespace Практическая_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+Console.Write("Введите трехзначное число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите число A: "); // Ввод числа A
+        int A = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Все цифры одинаковые: {Class1.Same(number)}");
+        Console.WriteLine($"Произведение цифр является трехзначным: {Class1.ProductThree(number)}");
+        Console.WriteLine($"Сумма цифр кратна A: {Class1.SumA(number, A)}");
+        }
+    }
+}
+
